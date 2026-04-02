726ed82606a7c7ae10a0094eeb4054606a0617a1
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2026-04-01 22:54:10 -0300

    incomplete factorial

---
+++ factorial.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUM_THREADS 3
+#define FACT 10
+
+long* fact_vector;
+
+typedef struct {
+    pthread_t id;
+    int num;
+    int section_start;
+    int section_end;
+} Thread_data;
+
+void* job(void* arg) {
+    Thread_data* thread = (Thread_data*) arg;
+    printf("\nInitializing T%d\t[%lld]...\n", thread->num, pthread_self());
+
+    for (int i = thread->section_start+1; i < thread->section_end; i++) {
+        fact_vector[thread->section_start] *= fact_vector[i];
+    }
+
+    if (thread->num == 0) {
+        int remaining = FACT % NUM_THREADS;
+
+        for (int i = NUM_THREADS - remaining - 1; i < NUM_THREADS; i++)
+        fact_vector[0] *= fact_vector[i];
+    }
+
+    return NULL;
+}
+
+int main() {
+    Thread_data threads[NUM_THREADS];
+
+    fact_vector = (long*) malloc(FACT * sizeof(long));
+
+    for (int i = 0; i < FACT; i++) {
+        fact_vector[i] = i+1;
+        printf("%ld", fact_vector[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads[i].num = i;
+        threads[i].section_start = (FACT / NUM_THREADS) * threads[i].num;
+        threads[i].section_end = (FACT / NUM_THREADS) * (threads[i].num + 1);
+        pthread_create(&threads[i].id, NULL, job, &threads[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i].id, NULL);
+    }
+
+    for (int i = 0; i < FACT; i++)
+        printf("%ld,", fact_vector[i]);
+
+    return 0;
+}
